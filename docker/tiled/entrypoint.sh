059a7feeb4d6cc9e1dad2a974d90e1c9679884c1
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2021-09-01 16:56:49 -0700

    don't use gunicorn

--- docker/tiled/entrypoint.sh
+++ docker/tiled/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 cp /secrets/tiled-auth.yml /deploy/config/
-/app/docker/check_config.py && exec gunicorn --config /deploy/gunicorn_config.py
+export PYTHONPATH=/deploy:$PYTHONPATH
+#/app/docker/check_config.py && exec gunicorn --config /deploy/gunicorn_config.py
+/app/docker/check_config.py && exec tiled serve config
