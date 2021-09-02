a63a24355d83fa8dfb3470c403a8353a2055cfb5
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2021-09-01 17:54:13 -0700

    Revert "don't use gunicorn"
    This reverts commit 059a7feeb4d6cc9e1dad2a974d90e1c9679884c1.

--- docker/tiled/entrypoint.sh
+++ docker/tiled/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 cp /secrets/tiled-auth.yml /deploy/config/
-export PYTHONPATH=/deploy:$PYTHONPATH
-#/app/docker/check_config.py && exec gunicorn --config /deploy/gunicorn_config.py
-/app/docker/check_config.py && exec tiled serve config
+/app/docker/check_config.py && exec gunicorn --config /deploy/gunicorn_config.py
