aab846125d379a901b64c0f914ae435913e4d3e4
Dylan McReynolds <40469975+dylanmcreynolds@users.noreply.github.com>
2021-10-01 14:14:39 -0700

    back to x-forward proxy headers in uvicorn

--- docker/tiled/gunicorn_config.py
+++ docker/tiled/gunicorn_config.py
@@ -9,5 +9,3 @@ worker_tmp_dir = "/dev/shm"
 errorlog = "-"
 accesslog = "-"
 loglevel = "warning"
-# add client's ip address
-access_log_format =  "%({X_Forwarded_For}i)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
