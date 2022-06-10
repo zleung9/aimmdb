86ba412300f04788d71d868346825139239ae2d3
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2022-06-10 12:31:12 -0700

    remove upstreamed routes

--- aimmdb/server/router_tiled.py
+++ aimmdb/server/router_tiled.py
@@ -1,22 +1,10 @@
-# NOTE these routes are all meant to be upstreamed into tiled
+# place for routes to be upstreamed into tiled
 import pydantic
 from fastapi import APIRouter, HTTPException, Request, Security
 from tiled.server.core import json_or_msgpack
 from tiled.server.dependencies import entry
 
 router = APIRouter()
-
-@router.delete("/node/delete/{path:path}")
-async def delete(
-    request: Request,
-    entry=Security(entry, scopes=["write:data", "write:metadata"]),
-):
-    if hasattr(entry, "delete"):
-        entry.delete()
-    else:
-        raise HTTPException(status_code=404, detail="entry does not support deletion")
-    return json_or_msgpack(request, None)
-
 
 # TODO /dataframe/partition
 # TODO /array/block
